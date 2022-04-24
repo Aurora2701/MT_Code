70602610df06088b696dd2ea42e48e993ed6cd78
Aurora Anna Pia Sergio <s290363@studenti.polito.it>
2022-04-24 15:54:11 +0300

    added todo notes

--- main.py
+++ main.py
@@ -4,11 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import pandas as pd
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def read_data():
@@ -31,35 +28,33 @@ def read_data():
 
 
 def drop_irrelevant_columns(df, col):
-    # remove columns that are not useful from the dataframe
+    # remove columns that are not useful from the dataframe [mostly for debugging purposes]
     df = df.drop(col, axis=1)
     print(df.info)
     return df
 
 
-def check_awe(row):  # check if a row of the dataset corresponds to an awe experience (true) or not (false)
+def is_awe(row):  # check if a row of the dataset corresponds to an awe experience (true) or not (false)
     if row['Mentally_challenged'] == 1 or row['All_at_once_struggle'] == 1:  # main component of awe is not experienced
         return False
-    #    coherence = row['Mentally_challenged'] - row['All_at_once_struggle']
-    #    if coherence <= -2 or coherence >= 2:  # respondent might have not put enough effort in the survey
-    #        return False
+
     count = 0  # how many of the important items were rated 1 ('Strongly disagree')
     for i in range(0, 12):
         if row[i] == 1:
             count = count + 1
-    # if we got to the for loop, it means neither of mentally_challenged or struggle were ones, so we have to reduce
+    # if we got to the for loop, it means neither of mentally_challenged or struggle were 1, so we have to reduce
     # our "max" count from 12 (total features) to 10. Therefore, if 6 or more of these aspects were rated as ones, maybe
     # the experience was not as intense as I would like to know...
     if count >= 6:
         return False
-    return True
+    return True  # passed both checks
 
 
 def clean_dataset(df):
-    indexes = []
+    indexes = []  # track rows to remove (not an awe experience)
     num_rows = df.shape[0]
     for i in range(0, num_rows):
-        if not check_awe(df.iloc[i]):
+        if not is_awe(df.iloc[i]):
             indexes.append(i)
     df = df.drop(indexes, axis=0)
     drop_awe = ['Time_change', 'Slowed_down', 'Smaller_self', 'Oneness_things', 'Humbling', 'Connected_humanity',
@@ -78,16 +73,20 @@ def generate_ratings(df):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data = read_data()
-    general_info = ['Timestamp', 'Age', 'Gender', 'Play_frequency', 'Study_videogames', 'Work_videogames', 'Comments1']
+    general_info = ['Timestamp', 'Comments1', 'Comments2']
     data = drop_irrelevant_columns(data, general_info)
 
     clean_data = clean_dataset(data)
 
-#    ratings = generate_ratings(clean_data)
-#    print(ratings.info())
+    # todo: check feature cross_correlation
+    # todo: create label column
+    # todo: decide how to treat each NaN
+    # todo: evaluate if you need to merge answers relative to the same game
+    fig, axes = plt.subplots()
+    corr = clean_data.corr(method='spearman', min_periods=0)
+    print(corr.shape)
+    sns.heatmap(corr, annot=True, mask=np.zeros_like(corr, dtype=bool), ax=axes)
+    plt.show()
 
-    # todo: reduce ratings so that every game appears only once, with the average of all the ratings per each column
-#    unique_games = clean_data.groupby(['Title'], axis=0, as_index=False)
-    unique_games = clean_data.agg()
-    print(unique_games.info())
+#
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
