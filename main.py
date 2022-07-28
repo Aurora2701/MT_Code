69affb06fef21a3aefaf67ea6b806f0fed591a10
Aurora Anna Pia Sergio <s290363@studenti.polito.it>
2022-07-28 16:40:45 +0200

    minor changes for final commit

--- main.py
+++ main.py
@@ -4,14 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import pandas as pd
-import sklearn.tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import KFold, cross_validate
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.feature_selection import SelectFromModel
-from sklearn.inspection import permutation_importance
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def read_data():
@@ -43,22 +41,21 @@ def drop_irrelevant_columns(df, col):
 def is_awe(row):
     # check if a row of the dataset corresponds to an awe experience (1) or not (0)
     if row['Mentally_challenged'] == 1 or row['All_at_once_struggle'] == 1:  # main component of awe is not experienced
-        return 0  # interesting note: result doesn't change if I use an AND instead of an OR
-    # EDIT: it does change... 125 ones with and, 108 with or
+        return 0
 
     count = 0  # how many of the relevant items were rated 1 ('Strongly disagree')
     for i in range(0, 12):  # the first 12 columns *of the second section* measure awe
         if row[i + 5] == 1:     # i + 5 to skip the answers of the first section
             count = count + 1
     # if we got to the for loop, it means neither of mentally_challenged or struggle were 1, so we have to reduce
-    # our "max" count from 12 (total features) to 10. Therefore, if 6 or more of these aspects were rated as ones, maybe
-    # the experience was not as intense as I would like to know...
+    # our "max" count from 12 (total features) to 10. Therefore, if 6 or more of these aspects were rated as ones
+    # the experience is not considered awe
     if count >= 6:
         return 0
     return 1  # passed both checks
 
 
-def lazy(row):  # uses column indexes
+def lazy(row):
     answer_streak = 1  # count how many consecutive answers *in the second section* are the same (straight-lining)
     for i in range(6, 20):
         if row[i] == row[i - 1]:
@@ -79,6 +76,11 @@ def check_pairs(row):
         return False
     if row['Main_character_rating'] != np.NaN and row['Main_char_descr'] == 'Not applicable':
         return False
+    if row['VR'] == 'No' and row['VR_descr'] != 'not applicable':
+        return False
+    if row['VR'] == 'Yes' and row['VR_descr'] == 'not applicable':
+        return False
+
     return True
 
 
@@ -116,14 +118,13 @@ def create_label(df):
 
 def replace(df):
     # first, remove the parenthesis because they were just supposed to help respondents
-    df.Genre = df.Genre.str.replace(' \([^)]*\)', '', regex=True)  # grazie stackOverflow
+    df.Genre = df.Genre.str.replace(' \([^)]*\)', '', regex=True)
 
     # second, convert yes and no into true and false (so we already have boolean types in some answers)
-    df = df.replace({'Study_videogames': 'Yes', 'Work_videogames': 'Yes'}, True)
-    df = df.replace({'Study_videogames': 'No', 'Work_videogames': 'No'}, False)
+    df = df.replace({'Study_videogames': 'Yes', 'Work_videogames': 'Yes', 'VR': 'Yes'}, True)
+    df = df.replace({'Study_videogames': 'No', 'Work_videogames': 'No', 'VR': 'No'}, False)
 
-    # now the part that I have to do because I ~fucked up~ intentionally wrote the answers in the poll with commas for
-    # legibility and ease of understanding ;)
+    # now change answers in the poll that contained commas for legibility and ease of understanding
     df.Main_char_descr = df.Main_char_descr.str.replace(', at least partially,', ' ', regex=True)
     df.Main_char_descr = df.Main_char_descr.str.replace(', to a certain extent', ' ', regex=True)
     df.Story_descr = df.Story_descr.str.replace('weak, contains plot holes', 'weak and containing plot holes')
@@ -135,38 +136,26 @@ def replace(df):
     df.Locations = df.Locations.str.replace('sea, ocean', 'sea/ocean')
     df.Locations = df.Locations.str.replace('space, spacecraft', 'space/spacecraft')
     df.Pace_and_difficulty = df.Pace_and_difficulty.str.replace('the game was challenging, sometimes too much',
-                                                                'the game was challenging - sometimes too much')
+                                                                'challenging')
 
-    # finally, replace NaNs (and VR ^^'):
-    df = df.replace({'Story_rating': np.nan, 'Main_character_rating': np.nan, 'VR': 'No'}, 0)
-    df = df.replace({'VR': 'Yes'}, True)
-    df = df.replace({'VR': 'No'}, False)
-    df = df.astype({'Story_rating': np.int64, 'Main_character_rating': np.int64, 'VR': np.int64})
+    # finally, replace NaNs:
+    df = df.replace({'Story_rating': np.nan, 'Main_character_rating': np.nan}, 0)
+    df = df.astype({'Story_rating': np.int64, 'Main_character_rating': np.int64})
 
     # print(df.info())
     return df
 
 
 def get_ratings(source, cols):
-    # Good argument for not grouping: even if the game is the same, different users might have different opinions
-    #                                 also, we have a problem now that I'm including gender and play frequency ^^'
-    # If grouping, ratings are apparently enough to give 100% accuracy already at depth 1
-    # If not grouping but with class_weight='balanced', the tree is shitty (probably overfitting) at any depth
     res = pd.DataFrame(source[cols])
 
-    objects = ['Gender', 'Play_frequency']      # title not a category for now
+    objects = ['Play_frequency']
     for obj in objects:
         awe_data[obj] = awe_data[obj].astype('category')
 
-    # add one-hot encoded [gender,] play frequency
+    # add one-hot encoded play frequency
     res = pd.concat([res, pd.get_dummies(source.iloc[:, 2], prefix='plays')], axis=1)
-    # res = pd.concat([res, pd.get_dummies(source.iloc[:, 1], prefix='is')], axis=1)
-    # res['label'] = source['Felt_awe']
-    # res = res.groupby(['Title'], sort=False).mean()     # drops title column automatically
-    # res[res['label'] >= 0.5] = 1
-    # res[res['label'] != 1] = 0
-    # res['label'] = res['label'].astype(np.int64)
-    res = res.drop('Title', axis=1)
+
     return res
 
 
@@ -179,31 +168,22 @@ def get_description_df(df, column):
     res = pd.DataFrame()
     tmp = pd.DataFrame(mlb.fit_transform(df[column]), columns=mlb.classes_, index=df.index)
     res = pd.concat([res, tmp], axis=1)
-    # res['label'] = df['Felt_awe']
     # print(res.info())
     return res
 
 
-def final_decision_tree(df, y, depth):
-    # creates the tree at the best depth, and prints feature importance - no, returns most important one(s)
-    n = df.shape[1] - 1     # -1 because out of n columns, from 0 to n-1, one is the label
+def final_decision_tree(df, y, depth, msl=1):
+    # creates the tree at the best depth, and returns most important features
+    n = df.shape[1]
     X = df.iloc[:, :n].to_numpy()
     y = y.to_numpy()
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=12)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+    # makes no sense to use k-fold coz I'm not looking for accuracy here, the feat are selected from one clf either way
 
-    clf = sklearn.tree.DecisionTreeClassifier(max_depth=depth, random_state=12, class_weight='balanced')
+    clf = DecisionTreeClassifier(max_depth=depth, min_samples_leaf=msl, random_state=12, class_weight='balanced')
     clf.fit(X_train, y_train)
 
-    print("Raw importances: ")
-    print(clf.feature_importances_)
-
-    # This would make sense with high-cardinality features, but my features are either 0-1 coz of 1-hot encoding, or on
-    # a scale from 0 to 5, so don't need it
-    # res = permutation_importance(clf, X, y, n_repeats=7, random_state=12)
-    # print("Averaged importances: ")
-    # print(res.importances_mean)
-
-    model = SelectFromModel(clf, prefit=True, threshold="mean")
+    model = SelectFromModel(clf, prefit=True, max_features=depth, threshold="1.5*mean")
     X_new = model.transform(X)      # reduce X to selected features
     support = model.get_support(indices=True)       # get indices of selected features
     features_names = []
@@ -211,24 +191,30 @@ def final_decision_tree(df, y, depth):
         features_names.append(df.columns[i])        # get names of selected features for columns
 
     important_features = pd.DataFrame(X_new, columns=features_names)        # df with selected features
+
+    plt.figure(figsize=[12.8, 7.2])
+    plot_tree(clf, feature_names=df.columns, class_names=["Non awe", "Awe"], label='none', impurity=False,
+              filled=True, proportion=True, rounded=True, fontsize=7)
+    plt.show()
     print(important_features.info())
+    print("Training accuracy: ", accuracy_score(y_train, clf.predict(X_train)))
+    print("Validation accuracy: ", accuracy_score(y_test, clf.predict(X_test)))
     return important_features
 
 
 def decision_trees(df, y, criterion='gini'):
-    # 1st attempt: just use ratings
-    # 2nd attempt: use ratings after group by - everything gives accuracy 1, which worries me
-    # 3rd attempt: use ratings + demographics - it's decent, although tr_acc=89% and val_acc=65%
-    n = df.shape[1] - 1
+    n = df.shape[1]
     tr_accuracies = {}
     val_accuracies = {}
     X = df.iloc[:, :n].to_numpy()
     y = y.to_numpy()
-    # x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2, random_state=12)
     kf = KFold(8)
 
-    for i in range(1, 10):      # max depth
-        clf = sklearn.tree.DecisionTreeClassifier(criterion=criterion, max_depth=i, random_state=12,
+    degrees = range(2, 15)
+    train = []
+    valid = []
+    for i in degrees:      # max depth - later min_samples_leaf
+        clf = DecisionTreeClassifier(criterion=criterion, min_samples_leaf=i, random_state=12,
                                                   class_weight='balanced')
         tr_accuracies[i] = []
         val_accuracies[i] = []
@@ -244,19 +230,19 @@ def decision_trees(df, y, criterion='gini'):
             val_accuracies[i].append(val_acc)
 
         avg_tr_acc = np.average(tr_accuracies[i])
+        train.append(avg_tr_acc)
         avg_val_acc = np.average(val_accuracies[i])
-        # print('Training and validation accuracies at depth ', i, ': ')     # Best depth before overfitting is 6-7
-        # print(tr_accuracies[i])
-        # print(val_accuracies[i])
-        # cm = confusion_matrix(y_test, y_test_pred)
-        # print('Confusion matrix at depth ', i, ': ')
-        # print(cm)
-        print('Average training accuracy at depth ', i, ': ', avg_tr_acc)
-        print('Average validation accuracy at depth ', i, ': ', avg_val_acc)
-    # cm = confusion_matrix(y_val, y_pred)
-    # print('Confusion matrix (default): ', cm)
-    # print(clf.feature_names_in_)
-    # print(clf.feature_importances_)
+        valid.append(avg_val_acc)
+        print('Average training accuracy at min_samples_leaf=', i, ': ', avg_tr_acc)
+        print('Average validation accuracy at min_samples_leaf=', i, ': ', avg_val_acc)
+
+    plt.plot(degrees, train, label='Train')
+    plt.plot(degrees, valid, label='Valid')
+    plt.legend(loc='upper left')
+    plt.xlabel('min_samples_leaf')
+    plt.ylabel('Accuracy')
+    plt.title('Ratings')
+    plt.show()
     return
 
 
@@ -270,10 +256,11 @@ if __name__ == '__main__':
     awe_data = replace(awe_data)  # adjust text answers
 
     # split dataset in ratings + demography vs other data
-    ratings_names = ['Title', 'Graphics_rating', 'Story_rating', 'Soundtrack_rating', 'Main_character_rating', 'VR']
+    ratings_names = ['Study_videogames', 'Work_videogames', 'Graphics_rating', 'Story_rating', 'Soundtrack_rating',
+                     'Main_character_rating', 'VR']
     ratings = get_ratings(awe_data, ratings_names)
-    print(ratings.info())
-    ratings_names.remove('Title')
+    # print(ratings.info())
+    # ratings_names.remove('Title')
     awe_data = drop_irrelevant_columns(awe_data, ratings_names)
 
     genre = get_description_df(awe_data, 'Genre')
@@ -287,57 +274,36 @@ if __name__ == '__main__':
 
     label = awe_data['Felt_awe']
 
-    # Time to create the stupid tree(s) and start feeding them data. NOTE: not random forest because I have no
-    # control over it, and it's difficult to interpret
-
-    # decision_trees(ratings, y=label)      # best max depth = 15
-    # decision_trees(genre, y=label)      # best max depth = 1
-    # decision_trees(graphics_description, y=label)   # best max depth = 2
+    # decision_trees(ratings, y=label)      # best max depth = 3, msl = 4/11 senza max depth
+    # decision_trees(genre, y=label, j=2, title='Genre')      # best max depth = 1
+    # decision_trees(graphics_description, y=label, j=3, title='Graphics')   # best max depth = 2
     # decision_trees(story_description, y=label)   # best max depth = 2
-    # decision_trees(soundtrack_description, y=label)   # best max depth = 2
-    # decision_trees(main_char_description, y=label)   # best max depth = 6
-    # decision_trees(locations, y=label)   # best max depth = 4
-    # decision_trees(pace_diff, y=label)   # best max depth = 4
+    # decision_trees(soundtrack_description, y=label, j=5, title='Soundtrack')   # best max depth = 2
+    # decision_trees(main_char_description, y=label, j=6, title='Main character')   # best max depth = 5, msl indifferent
+    # decision_trees(locations, y=label, j=7, title='Locations')   # best max depth = 2;4
+    # decision_trees(pace_diff, y=label, j=8, title='Pace and difficulty')   # best max depth = 4
     # decision_trees(vr_descr, y=label)   # best max depth = 1 and same results (78% tr and val) until 20 so idk maybe
     # i should drop it
 
     important_features_df = pd.DataFrame()
-    important_features_df = pd.concat([important_features_df, final_decision_tree(ratings, depth=15, y=label)], axis=1)
+    important_features_df = pd.concat([important_features_df, final_decision_tree(ratings, depth=10, msl=11, y=label)],
+                                      axis=1)
     important_features_df = pd.concat([important_features_df, final_decision_tree(genre, depth=1, y=label)], axis=1)
     important_features_df = pd.concat(
-        [important_features_df, final_decision_tree(graphics_description, depth=2, y=label)], axis=1)
+        [important_features_df, final_decision_tree(graphics_description, msl=2, y=label)], axis=1)
     important_features_df = pd.concat(
         [important_features_df, final_decision_tree(story_description, depth=2, y=label)], axis=1)
     important_features_df = pd.concat(
         [important_features_df, final_decision_tree(soundtrack_description, depth=2, y=label)], axis=1)
     important_features_df = pd.concat(
-        [important_features_df, final_decision_tree(main_char_description, depth=6, y=label)], axis=1)
+        [important_features_df, final_decision_tree(main_char_description, depth=4, msl=5, y=label)], axis=1)
     important_features_df = pd.concat([important_features_df, final_decision_tree(locations, depth=4, y=label)], axis=1)
     important_features_df = pd.concat([important_features_df, final_decision_tree(pace_diff, depth=4, y=label)], axis=1)
+    # important_features_df = pd.concat([important_features_df, final_decision_tree(vr_descr, depth=1, y=label)], axis=1)
 
     print(important_features_df.info())
-
-    decision_trees(important_features_df, y=label)  # overfitting starts at depth 3
-    final_decision_tree(important_features_df, depth=2, y=label)    # 65% val accuracy
-
-    # final_decision_tree(ratings, depth=15)
-    # final_decision_tree(graphics_description, depth=2)
-    # final_decision_tree(story_description, depth=2)
-    # final_decision_tree(soundtrack_description, depth=2)
-    # final_decision_tree(main_char_description, depth=6)
-    # final_decision_tree(locations, depth=4)
-    # final_decision_tree(pace_diff, depth=4)
-    # final_decision_tree(vr_descr, depth=1)       # probably should drop it, important features are 1 thing and na
-
-    # todo but it's just a reminder: see accuracy of big df (supposed to be good). Features are the most informative for
-    #  the ML model, but see average values in each column to understand in what way they are important (better if they'
-    #  re there or not? If rating avg is 4 -> good, if 1 hot thingy avg is 0.002, means it's probably there by accident)
-    # From confusion matrices, the 'best' splitter seems better than the random one, having fewer mispredictions in the
-    # second row (5-29 vs 3-31), so I'm dropping that already
-
-    # done: use select from model. Also, use it multiple times - because trees don't work well with too many features,
-    #       split them in several rating + descriptions tables, and then find feature importance. Finally, test overall
-    #       accuracy with the most important features from each group
+#    decision_trees(important_features_df, y=label)  # overfitting starts at depth 3
+#    final_decision_tree(important_features_df, depth=2, y=label)    # 65% val accuracy
 
 #
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
